b3a502614dc5f6c01bc9c80c0132ebd77dc1aa9a
Jerome Gero <JeromeRGero@gmail.com>
2022-12-01 09:35:09 -0500

    Update mod.rs
    minor reformat.

--- src/restaurant/mod.rs
+++ src/restaurant/mod.rs
@@ -1,11 +1,9 @@
-
-
 mod pizza_order {
     pub struct Pizza {
         pub dough: String,
         pub cheese: bool,
         pub topping: String,
-        pub size_in_oz: f32 
+        pub size_in_oz: f32,
     }
 
     impl Pizza {
@@ -14,7 +12,7 @@ mod pizza_order {
                 dough: String::from("no-dough"),
                 cheese: false,
                 topping: String::from("no"),
-                size_in_oz: 0.0
+                size_in_oz: 0.0,
             }
         }
         pub fn lunch(topping: &str) -> Pizza {
@@ -22,7 +20,7 @@ mod pizza_order {
                 dough: String::from("pizza-dough"),
                 cheese: true,
                 topping: String::from(topping),
-                size_in_oz: 28.2
+                size_in_oz: 28.2,
             }
         }
         pub fn dinner(cheese: bool, topping: &str, size_in_oz: f32) -> Pizza {
@@ -30,7 +28,7 @@ mod pizza_order {
                 dough: String::from("pizza-dough"),
                 cheese: cheese,
                 topping: String::from(topping),
-                size_in_oz: size_in_oz.to_owned()
+                size_in_oz: size_in_oz.to_owned(),
             }
         }
     }
@@ -43,22 +41,35 @@ mod pizza_order {
         }
         pub fn take_order(meal: Meal) {
             seat_at_table();
-            if meal.is_pizzatime() {
-                let cust_piz = match meal {
-                    Meal::Lunch => Ok(super::Pizza::lunch("veggies")),
-                    Meal::Dinner => Ok(super::Pizza::dinner(true, "pepper", 33.3)),
-                    Meal::Breakfast => Err("System Error: Contact Admin - \"Breakfast isn't a time for pizza...\"")
-                };
-                serve_customer(cust_piz.unwrap());
-            }
+            let cust_piz = match meal {
+                Meal::Lunch => Ok(super::Pizza::lunch("veggies")),
+                Meal::Dinner => Ok(super::Pizza::dinner(true, "pepper", 33.3)),
+                Meal::Breakfast => {
+                    Err("System Error: Contact Admin - \"Breakfast isn't a time for pizza...\"")
+                }
+            };
+            let mut pizza = match cust_piz {
+                Ok(pizza) => pizza,
+                Err(error) => panic!("Problem creating pizza: {:?}", error),
+            };
+            serve_customer(pizza);
         }
         fn serve_customer(cust_piz: super::Pizza) {
             println!(
                 "The Customer is served a {}oz {} pizza with{} cheeze and {} toppings.",
-                cust_piz.size_in_oz, 
-                cust_piz.dough, 
-                if cust_piz.cheese {String::from("")} else {String::from("out")}, 
-                if cust_piz.topping.is_empty() {String::from("no")} else {cust_piz.topping});
+                cust_piz.size_in_oz,
+                cust_piz.dough,
+                if cust_piz.cheese {
+                    String::from("")
+                } else {
+                    String::from("out")
+                },
+                if cust_piz.topping.is_empty() {
+                    String::from("no")
+                } else {
+                    cust_piz.topping
+                }
+            );
         }
     }
 }
@@ -67,14 +78,14 @@ pub mod meal {
     pub enum Meal {
         Breakfast,
         Lunch,
-        Dinner
+        Dinner,
     }
 
-    impl Meal {  
+    impl Meal {
         pub fn is_pizzatime(&self) -> bool {
             match self {
                 Meal::Lunch | Meal::Dinner => true,
-                _ => false
+                _ => false,
             }
         }
     }
